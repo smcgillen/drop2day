9bec752b3b8bfe51ce1f4bef06317267e2f99a6e
Stephen McGillen <smcgillen@hotmail.com>
2013-09-05 10:04:27 +1000

    styles

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.2.13'
 
 gem 'pg', '~> 0.15.1'
 gem 'instagram'
+gem 'zurb-foundation'
 
 
 # Gems used only for assets and not required
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require_tree .
 //= require instagram
+
+$(function(){ $(document).foundation(); });
