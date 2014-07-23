1d2f81ae5f180538044422cbbcf9230ed283bac8
Stephen McGillen <smcgillen@hotmail.com>
2014-07-23 17:52:39 +1000

    Domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def set_domain
+    if Rails.env.development?
+      @domain_name = 'localhost:3000'
+    else
+      @domain_name = 'drop2day.herokuapp.com/'
+    end
+  end
 end
--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -6,11 +6,11 @@ class InstagramController < ApplicationController
   end
 
   def connect
-    redirect_to Instagram.authorize_url(:redirect_uri => 'http://localhost:3000/callback', :scope => 'relationships')
+    redirect_to Instagram.authorize_url(:redirect_uri => "http://#{@domain_name}/callback", :scope => 'relationships')
   end
 
   def callback
-    response = Instagram.get_access_token(params[:code], :redirect_uri => 'http://localhost:3000/callback')
+    response = Instagram.get_access_token(params[:code], :redirect_uri => "http://#{@domain_name}/callback")
     session[:access_token] = response.access_token
     redirect_to '/success'
   end
